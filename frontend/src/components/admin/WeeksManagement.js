9454f93237b4bb148a841965e1d8002c64f393d1
Adiel Klein <adielklein@gmail.com>
2025-12-21 15:11:53 +0200

    עדכון קוד

--- frontend/src/components/admin/WeeksManagement.js
+++ frontend/src/components/admin/WeeksManagement.js
@@ -780,10 +780,10 @@ function WeeksManagement({ selectedWeek: parentSelectedWeek, onWeekSelect }) {
                     <div 
                       style={{
                         position: 'absolute',
-                        left: '100%',
+                        right: '100%',
                         top: 0,
                         width: '200px',
-                        marginLeft: '-2px',
+                        marginRight: '-2px',
                         backgroundColor: 'white',
                         border: '1px solid #dee2e6',
                         borderRadius: '4px',
